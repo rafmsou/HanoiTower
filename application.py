97c69db05db8e533f6ae71a19d98c9c17a83bb3c
Rafael Sousa <rafmsou@gmail.com>
2015-09-24 00:44:32 -0300

    Discs configuration

--- application.py
+++ application.py
@@ -6,6 +6,14 @@ class Application(tk.Frame):
     def __init__(self, master=None):
         tk.Frame.__init__(self, master)
         self.grid()
+
+        #disc configuration
+        self.discsLength = 7
+        self.discWidthDiameterStart = 40
+        self.discWidthDiameterOffset = 10
+        self.discHeightDiameter = 20
+
+
         self.createWidgets()
 
     def createWidgets(self):
@@ -23,23 +31,53 @@ class Application(tk.Frame):
 
         self.canvas.bind('<Motion>', self.print_current_coords)
 
-        t = Thread(target=self.move_rect)
-        t.start()
+        self.canvas.disc1 = self.create_disc(195, 130, 40, 20)
+        self.canvas.disc2 = self.create_disc(190, 145, 50, 20)
+        self.canvas.disc3 = self.create_disc(185, 160, 60, 20)
+        self.canvas.disc4 = self.create_disc(180, 175, 70, 20)
+        self.canvas.disc5 = self.create_disc(175, 190, 80, 20)
+        self.canvas.disc6 = self.create_disc(170, 205, 90, 20)
+        self.canvas.disc7 = self.create_disc(165, 220, 100, 20)
+
+        self.canvas.disc11 = self.create_disc(305, 130, 40, 20)
+        self.canvas.disc12 = self.create_disc(300, 145, 50, 20)
+        self.canvas.disc13 = self.create_disc(295, 160, 60, 20)
+        self.canvas.disc14 = self.create_disc(290, 175, 70, 20)
+        self.canvas.disc15 = self.create_disc(285, 190, 80, 20)
+        self.canvas.disc16 = self.create_disc(280, 205, 90, 20)
+        self.canvas.disc17 = self.create_disc(275, 220, 100, 20)
+
+        self.canvas.disc11 = self.create_disc(415, 130, 40, 20)
+        self.canvas.disc12 = self.create_disc(410, 145, 50, 20)
+        self.canvas.disc13 = self.create_disc(405, 160, 60, 20)
+        self.canvas.disc14 = self.create_disc(400, 175, 70, 20)
+        self.canvas.disc15 = self.create_disc(395, 190, 80, 20)
+        self.canvas.disc16 = self.create_disc(390, 205, 90, 20)
+        self.canvas.disc17 = self.create_disc(385, 220, 100, 20)
+
+        #t = Thread(target=self.move_rect)
+        #t.start()
 
     def print_current_coords(self, event):
         x = self.canvas.canvasx(event.x)
         y = self.canvas.canvasy(event.y)
         self.coordsLabelValue.set('x: {} y: {}'.format(x, y))
 
+    def create_disc(self, x, y, width, height):
+        x0 = x
+        y0 = y
+        x1 = x + width
+        y1 = y + height
+
+        disc = self.canvas.create_oval(x0, y0, x1, y1)
+        return disc
+
     def move_rect(self):
-
-        o = self.canvas.create_oval(170, 225, 250, 240)
-
-        # for i in xrange(1, 100):
-        #     self.canvas.move(o, -1, 0)
-        #     self.canvas.update()
-        #     time.sleep(0.025)self.quit()
-
+        for i in xrange(1, 100):
+            self.canvas.move(o, -1, 0)
+            self.canvas.update()
+            time.sleep(0.025)
+            self.quit()
 
 app = Application()
 app.master.title('Sample application')
