684f7c9408de2715736c95d23a901fdd3f834d39
Rafael Sousa <rafmsou@gmail.com>
2015-09-30 00:02:12 -0300

    Lógica do movimento dos discos

--- application.py
+++ application.py
@@ -1,5 +1,6 @@
 import Tkinter as tk
 import time
+import math
 from threading import Thread
 
 class Application(tk.Frame):
@@ -12,9 +13,13 @@ class Application(tk.Frame):
         self.discsLength = 7
 
         #tower configuration
-        self.towersBasePoint = [[165, 219],[275, 219],[385, 219]]
+        self.towersLocation = [[216, 219],[326, 219],[437, 219]]
 
         self.createWidgets()
+
+    def start(self):
+        t = Thread(target=self.moveDiscToTower, args=(self.discs[6], 2))
+        t.start()
 
     def initializeDiscs(self):
         baseWidthDiameter = 100
@@ -25,10 +30,11 @@ class Application(tk.Frame):
         xOffset = 5
         yOffset = 10
 
-        x, y = self.towersBasePoint[1]
+        x, y = self.towersLocation[1]
+        x -= (baseWidthDiameter / 2)
 
         for i in range(0, self.discsLength):
-            self.discs.append(self.create_disc(x, y, widthDiameter, heightDiameter))
+            self.discs.append(self.createDisc(x, y, widthDiameter, heightDiameter))
             x += xOffset
             y -= yOffset
             widthDiameter -= widthDiameterOffset
@@ -36,6 +42,10 @@ class Application(tk.Frame):
     def createWidgets(self):
         self.canvas = tk.Canvas(self, height = 350, width = 600)
         self.canvas.grid(column = 0)
+
+        self.startButton = tk.Button(self, text='Start',command=self.start)
+        self.startButton.grid(column = 1)
+
         self.quitButton = tk.Button(self, text='Quit',command=self.quit)
         self.quitButton.grid(column = 1)
 
@@ -46,19 +56,17 @@ class Application(tk.Frame):
         self.photo = tk.PhotoImage(file = './discs_base.gif')
         self.canvas.create_image(330,200, image=self.photo)
 
-        self.canvas.bind('<Motion>', self.print_current_coords)
+        self.canvas.bind('<Motion>', self.printCurrentCoords)
 
         self.initializeDiscs()
 
-        #t = Thread(target=self.move_rect)
-        #t.start()
 
-    def print_current_coords(self, event):
+    def printCurrentCoords(self, event):
         x = self.canvas.canvasx(event.x)
         y = self.canvas.canvasy(event.y)
         self.coordsLabelValue.set('x: {} y: {}'.format(x, y))
 
-    def create_disc(self, x, y, width, height):
+    def createDisc(self, x, y, width, height):
         x0 = x
         y0 = y
         x1 = x + width
@@ -67,7 +75,7 @@ class Application(tk.Frame):
         disc = self.canvas.create_oval(x0, y0, x1, y1)
         return disc
 
-    def move_disc(self, disc, direction, amount):
+    def moveDisc(self, disc, direction, amount):
 
         for i in xrange(1, amount):
             if direction == 'left':
@@ -79,8 +87,28 @@ class Application(tk.Frame):
             elif direction == 'down':
                 self.canvas.move(disc, 0, 1)
 
-            self.canvas.update()
+            #self.canvas.update()
             time.sleep(0.025)
+
+    def moveDiscToTower(self, disc, tower):
+        discCurrentTower = 1
+        discDestinationTower = tower
+        #discDestinationPosition = len(tower) + 1
+        discDestinationPosition = 1
+        upLimit = 110
+        discX0, discY0, discX1, discY1 = self.canvas.coords(disc)
+        discWidth = discX1 - discX0
+        towerX, towerY = self.towersLocation[tower]
+
+        upAmount = int(discY0 - upLimit)
+        moveAmount = int(math.fabs(towerX - discX0) - (discWidth / 2))
+        moveDirection = 'right' if discCurrentTower < discDestinationTower else 'left'
+        downAmount = towerY - upLimit
+
+        self.moveDisc(disc, 'up', upAmount)
+        self.moveDisc(disc, moveDirection, moveAmount)
+        self.moveDisc(disc, 'down', downAmount)
+
 
 app = Application()
 app.master.title('Sample application')
