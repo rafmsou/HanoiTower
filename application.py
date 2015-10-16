4310653d9d5934fdc61c3b5c50333ac911247aba
Rafael Sousa <rafmsou@gmail.com>
2015-10-16 00:18:45 -0300

    Layout Fix

--- application.py
+++ application.py
@@ -8,25 +8,25 @@ class Application(tk.Frame):
         self.createWidgets()
 
     def createWidgets(self):
-        self.resetButton = tk.Button(self, text='Reiniciar', command=self.resetStage, width=5)
-        self.resetButton.grid(row=0, column=0)
+        self.resetButton = tk.Button(self, text='Reiniciar', command=self.resetStage, width=7)
+        self.resetButton.grid(row=1, column=2, sticky=tk.E)
 
-        self.moveButton = tk.Button(self, text='Move', command=self.moveAction, width=5)
-        self.moveButton.grid(row=0, column=1)
+        self.moveButton = tk.Button(self, text='Mover', command=self.moveAction, width=7)
+        self.moveButton.grid(row=2, column=2, sticky=tk.E)
 
         self.coordsLabelValue = tk.StringVar()
         self.coordsLabel = tk.Label(self, textvariable=self.coordsLabelValue)
-        self.coordsLabel.grid(row=2, columnspan=2)
+        self.coordsLabel.grid(row=3, columnspan=5)
 
-        self.quitButton = tk.Button(self, text='Fechar',command=self.closeAction, width=5)
-        self.quitButton.grid(row=3, column=1)
+        self.quitButton = tk.Button(self, text='Fechar',command=self.closeAction, width=7)
+        self.quitButton.grid(row=4, column=4, sticky=tk.E)
 
         self.resetStage()
 
 
     def resetStage(self):
         self.canvas = tk.Canvas(self, height=350, width=600)
-        self.canvas.grid(row=1, column=0)
+        self.canvas.grid(row=0, columnspan=5, sticky=tk.W)
         self.photo = tk.PhotoImage(file='./discs_base.gif')
         self.canvas.create_image(330,200, image=self.photo)
         self.canvas.bind('<Motion>', self.printCurrentCoords)
